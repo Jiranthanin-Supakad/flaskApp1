c4a198f8550837f24dcaf087ad16e344431e28d2
Jiranthanin-Supakad <neen7899@gmail.com>
2023-01-17 15:13:21 +0700

    lab09

--- app/__init__.py
+++ app/__init__.py
@@ -5,4 +5,6 @@ app = Flask(__name__, static_folder='static')
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = '4c4e82067c705665274f8d1d33d5fd847622416172c2f7ef'
  
+app.config['JSON_AS_ASCII'] = False
+
 from app import views  # noqa
